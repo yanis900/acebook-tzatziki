d389854860af4d21c7c8736b1dab65afe2602ddb
John Forster <john.forster@makers.tech>
2024-08-01 16:07:09 +0100

    Fix linting errors

--- frontend/src/components/LogoutButton.jsx
+++ frontend/src/components/LogoutButton.jsx
@@ -1,7 +1,6 @@
-import React from "react";
 import { useNavigate } from "react-router-dom";
 
-function LogoutButton(props) {
+function LogoutButton() {
   const navigate = useNavigate();
 
   function logOut() {
