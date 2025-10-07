62f83590b94c1a14db797beeba4b9666b0b75c71
stavroula <nika.stavroula@gmail.com>
2025-10-07 14:27:10 +0100

    Co-authored-by: CodeByNikita <CodeByNikita@users.noreply.github.com> Co-authored-by: Yanis Aityounes <yanis900@users.noreply.github.com>

--- frontend/src/pages/Feed/FeedPage.jsx
+++ frontend/src/pages/Feed/FeedPage.jsx
@@ -7,13 +7,14 @@ import LogoutButton from "../../components/LogoutButton";
 
 export function FeedPage() {
   const [posts, setPosts] = useState([]);
+  const [message, setMessage] = useState('')
   const navigate = useNavigate();
 
   useEffect(() => {
     const token = localStorage.getItem("token");
     const loggedIn = token !== null;
     if (loggedIn) {
-      getPosts(token)
+     getPosts(token) 
         .then((data) => {
           setPosts(data.posts);
           localStorage.setItem("token", data.token);
@@ -25,16 +26,46 @@ export function FeedPage() {
     }
   }, [navigate]);
 
-  const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token");
   if (!token) {
     navigate("/login");
     return;
   }
 
+const handleSubmit = async () => {
+  const url = 'http://localhost:3000/posts'
+  try {
+    console.log(token)
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({message: message})
+    })
+
+    const data = await response.json()
+    console.log(message)
+    console.log(data)
+
+    return data
+  } catch (error) {
+    console.log(error)
+  }
+
+}
+
   return (
     <>
-      <h2>Posts</h2>
+      <h2>Feed Page</h2>
       <div className="feed" role="feed">
+        <label>
+            <input name="Post" onChange={(e) => setMessage(e.target.value)} placeholder="What's on your mind??" />
+        </label>
+        <label>
+            <button onClick={handleSubmit}>:-8</button>
+        </label>
         {posts.map((post) => (
           <Post post={post} key={post._id} />
         ))}
@@ -43,3 +74,24 @@ export function FeedPage() {
     </>
   );
 }
+
+
+// export default function Form() {
+//   const [age, setAge] = useState('20');
+//   const ageAsNumber = Number(age);
+//   return (
+//     <>
+//       <label>
+//         <input name="Post" defaultValue="What's on your mind??" />
+//         <button onClick={() => setAge(ageAsNumber + 10)}>
+//           Add 10 years
+//         </button>
+//       </label>
+//       {posts.map((post) => (
+//           <Post post={post} key={post._id} />
+//         ))}
+//       </div>
+//       <LogoutButton />
+//     </>
+//   );
+// }
