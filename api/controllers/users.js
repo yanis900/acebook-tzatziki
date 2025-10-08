0513f225fe6faf61761e69f5ccd8866ac45bec1c
yanis900 <yanis991@hotmail.com>
2025-10-08 16:14:39 +0100

    Add getUser to user controller

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,6 +1,22 @@
 const User = require("../models/user");
+const { generateToken } = require("../lib/token");
 
-function create(req, res) {
+async function getUser(req, res) {
+  const email = req.query.email
+
+  const user = await User.findOne({ email: email });
+
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+
+  const safeUser = user.toObject();
+  delete safeUser.password;
+  
+  res.status(200).json({ user: safeUser });
+}
+
+async function create(req, res) {
   const firstname = req.body.firstname;
   const lastname = req.body.lastname;
   const email = req.body.email;
@@ -21,6 +37,7 @@ function create(req, res) {
 
 const UsersController = {
   create: create,
+  getUser: getUser
 };
 
 module.exports = UsersController;
