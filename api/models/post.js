f1c6d03b556a196d5f80f159de443ecc87188eb2
yanis900 <yanis991@hotmail.com>
2025-10-09 23:42:55 +0100

    Update Post model to include created date

--- api/models/post.js
+++ api/models/post.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 // A Schema defines the "shape" of entries in a collection. This is similar to
 // defining the columns of an SQL Database.
 const PostSchema = new mongoose.Schema({
-  message: String,
-  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  message: { type: String, required: true },
+  date: { type: Date, default: Date.now },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 });
 
 // We use the Schema to create the Post model. Models are classes which we can
