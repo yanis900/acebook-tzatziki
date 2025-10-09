6e7ac12dcc964d9c45a81d44511d9eeee5ae807a
stavroula <nika.stavroula@gmail.com>
2025-10-09 16:21:28 +0100

    Created LoginButton.jsx and SignupButton.jsx

---
+++ frontend/src/components/SignupButton.jsx
@@ -0,0 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
+function SignupButton() {
+  const navigate = useNavigate();
+
+  function goToSignup() {
+    navigate("/signup");
+  }
+
+  return <button onClick={goToSignup}>Sign Up</button>;
+}
+
+export default SignupButton;
