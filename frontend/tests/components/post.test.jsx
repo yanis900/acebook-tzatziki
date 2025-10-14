cc757dd5e40aba013772d4188891c59d1bcf9431
yanis900 <yanis991@hotmail.com>
2025-10-14 16:19:48 +0100

    Fix tests

--- frontend/tests/components/post.test.jsx
+++ frontend/tests/components/post.test.jsx
@@ -3,11 +3,11 @@ import { render, screen } from "@testing-library/react";
 import Post from "../../src/components/Post";
 
 describe("Post", () => {
-  test("displays the message as an article", () => {
+  test("displays the message as an article", async () => {
     const testPost = { _id: "123", message: "test message", date: "2025-10-10T11:37:04.662Z" };
     render(<Post post={testPost} />);
 
-    const article = screen.getByRole("article");
-    expect(article.textContent).toBe("test message - 3 days ago");
+    const post = await screen.findByText(/test message.*4 days ago/, { exact: false });
+    expect(post).toBeDefined()
   });
 });
