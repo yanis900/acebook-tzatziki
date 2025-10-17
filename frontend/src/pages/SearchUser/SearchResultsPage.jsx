cab65bc8af00d3b856c273ddc936d0dc6124f32f
stavroula <nika.stavroula@gmail.com>
2025-10-17 12:12:43 +0100

    - Changed colour and text position of the users name

--- frontend/src/pages/SearchUser/SearchResultsPage.jsx
+++ frontend/src/pages/SearchUser/SearchResultsPage.jsx
@@ -87,7 +87,7 @@ export function SearchResultsPage() {
             <span>No users found matching your search query.</span>
           </div>
         ) : (
-          <div className="space-y-4">
+          <div className="space-y-4 text-left">
             {results.map((user) => (
               <div 
                 key={user._id} 
@@ -109,7 +109,8 @@ export function SearchResultsPage() {
                 <div className="flex-1">
                   <a 
                     href={`/profile/${user.firstname}-${user.lastname}-${user._id.slice(-6)}`}
-                    className="text-l font-semibold link link-hover text-[#2B98BA] hover:text-[#4DBCDB]"
+                    className="text-xl font-semibold link link-hover text-[#2B98BA] hover:text-[#4DBCDB]"
+                    style={{color: "#2B98BA"}}
                   >
                     {user.firstname} {user.lastname}  
                   </a>
